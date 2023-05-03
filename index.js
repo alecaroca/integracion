d1a59a6aa86baf9c2855b9e330302e5734f9a7be
alecaroca <ale.caroca@duocuc.cl>
2023-05-03 19:31:24 -0400

    ajuste a tabla

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const API_URL="https://jsonplaceholder.typicode.com/users"
-const HTMLResponse= document.querySelector("app");
-
-/* $.get(API_URL, function(respuesta){
-    console.log(respuesta)
-    respuesta.forEach(function(item){
-        console.log(item)
-      
-        $("#app").text(item)
-    })
-
-    const tpl = users.map((user) => document.write(user.name+ " "+ user.email+ "<br>"));
-},"json") */
-
-var usuarios;
+const HTMLResponse = document.querySelector("#app");
 
 fetch(API_URL)
     .then((response)=> response.json())
     .then((users)=>{
         console.log(users)
-        const tpl = users.map((user) => user.name +" "+ user.email +"<br>");
-        $("#app").text(tpl)
-        /* $("#app").text(usuarios) */
-        
+        const usuarios = users.map((user) => `<tr><td class="text-center">${user.name}</td><td class="text-center">${user.email}</td><td class="text-center">${user.phone}</td><td class="text-center">${user.website}</td></tr>`)
+        HTMLResponse.innerHTML = usuarios
     });
