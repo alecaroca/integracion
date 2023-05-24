8e473191e59e352a5bb457ae7d5f700e680f3d05
alecaroca <ale.caroca@duocuc.cl>
2023-05-24 15:53:40 -0400

    sda

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const API_URL='https://jsonplaceholder.typicode.com/users'
+const API_URL2='https://alecaroca-vigilant-fishstick-4pr9vw954qwh7qxj-8000.preview.app.github.dev/Cliente'
 const aplication = document.querySelector('.app');
 
 /* fetch(API_URL)
@@ -12,12 +13,28 @@ const aplication = document.querySelector('.app');
     fetch(API_URL)
         .then(response=> response.json())
         .then(data => {
+            console.log(data)
             let body = '';
             data.forEach(data => {
                 body += `<tr><td>${data.id}</td><td>${data.name}</td><td>${data.email}</td></tr>`
             });
-            
+            console.log(data)
             document.getElementById('app').innerHTML = body
+        })
+        .catch(error => console.log(error))
+    
+    fetch('https://alecaroca-vigilant-fishstick-4pr9vw954qwh7qxj-8000.preview.app.github.dev/Cliente', {
+        
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+     
+      })
+        .then(response=> response.json())
+        .then(data2 => {
+            console.log(data2)
+            
+        
         })
         .catch(error => console.log(error))
 
